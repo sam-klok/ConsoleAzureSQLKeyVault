3dec53d3ed44bcb0751646c8b57b07b63b60a6eb
sam-klok <sklok11@gmail.com>
2021-08-01 20:05:53 -0700

    Initial commit

---
+++ ConsoleAzureSQLKeyVault/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Security.Cryptography;
+//using Azure.Identity;
+//using Microsoft.Data.SqlClient;
+//using Microsoft.Data.SqlClient.AlwaysEncrypted.AzureKeyVaultProvider;
+
+
+namespace ConsoleAzureSQLKeyVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Another attempt to use Azure KeyVault to connect to the Azure MS SQL Database
+
+... work in progress...
+sam
